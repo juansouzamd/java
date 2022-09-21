5c0131b7a094b0a68dea5e5fda6b5ab091bdef46
juansouzamd <juansouzamd@gmail.com>
2022-09-21 00:35:44 -0300

    while

---
+++ src/crescente.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class crescente {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+    int x = in.nextInt();
+    int y = in.nextInt();
+        
+        while (x != y) {
+
+            if (x < y) {
+                System.out.println(x + " - "+ y + " Crescente");
+            } else {
+                 System.out.println(x + " - "+ y + " decrescente");
+            }
+
+            x = in.nextInt();
+            y = in.nextInt();
+        }
+    }
+
+}
