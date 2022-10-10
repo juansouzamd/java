aede53acee23c4c60371831313743cfd6eea9156
juansouzamd <juansouzamd@gmail.com>
2022-10-09 23:27:33 -0300

    adicionando pastas

---
+++ java02/src/vetorImpares.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class vetorImpares {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+    System.out.println("Digite a quantidade de numeros que deseja armazenar");
+        int array = in.nextInt();
+
+        int [] vetor = new int [array];
+
+    for (int i = 0; i < array; i++){
+        System.out.println("Digite o numero");
+        vetor[i] = in.nextInt();
+             
+    }
+
+       for (int j = 0; j < array; j++){
+        
+        if (vetor[j] % 2 != 0){
+            System.out.println(vetor[j]);
+        }
+       }
+
+
+
+    in.close();
+    }
+}
