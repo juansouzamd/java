308bc4c24300bfb03ed2a6aeba6630907898f43f
juansouzamd <juansouzamd@gmail.com>
2022-10-26 17:34:38 -0300

    desafio

--- Desafios/src/vetor.java
+++ Desafios/src/vetor.java
@@ -17,51 +17,52 @@ class vetor {
 
         System.out.println();
 
-        for (int j = 0; j < vetor; j++){
+        for (int num : numeros){
         
-            System.out.println((int)numeros[j]);
-
+            System.out.println(num);
         }
-
 
         System.out.println();
 
-        int valorNumero = 0;
-        int soma = 0;
-        int num = 0;
-        int vezes = 0;
-        int quebra = 0;
-        int p;
-        int novoArray [] = new int [vetor]; 
-
-        for (p = 0; p < vetor; p++){
-
-            soma = valorNumero - num;
-
-            if (soma == 1){
-                vezes++;
-
-                novoArray[p] = valorNumero;
-
-                
-            } else if (soma > 1 || soma < 1) {
-                quebra++;
-            }
-            
-            num = valorNumero;
-            valorNumero = numeros[p];
-    
-           
-        }
-
-        for (int l = 0; l < vetor; l++){
         
-            System.out.println(novoArray[l]+ "novo array");
-        }
+        int num = numeros[0];
+        int array = 0;
+        int contador = 1;
 
-        System.out.println(vezes + "vezes que caiu no == 1");
-        System.out.println(quebra+ "quebras");
+
+  
+
+            for (int i = 0; i < vetor; i++){
+
+               if(numeros[contador] - numeros[i] == 1){
+
+                    
+
+               } 
+
+                array = numeros[i];
+                int valor = array - num;
+                
+    
+                if (valor == 1){
+                    contador++;
+                }
+    
+                num = array;
+            }
+
+
+
+  
+
+       /* System.out.println();
+
+        for (int sequencia : seq){
+            System.out.println(sequencia);
+        }*/
+
 
         in.close();
-    }
+   
+}
 }
