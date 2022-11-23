07bb128c205c25a7c9dd81603e5c0bd83fbc2897
juansouzamd <juansouzamd@gmail.com>
2022-11-23 15:01:59 -0300

    Ex01 ado13

--- SenacADO13/src/Ex01.java
+++ SenacADO13/src/Ex01.java
@@ -1,15 +1,56 @@
 import java.util.*;
 
 public class Ex01 {
+
     public static void main(String[] args) {
-        Scanner ler = new Scanner (System.in);
+        Scanner ler = new Scanner(System.in);
 
-    System.out.println("===== Exercício 1 =====");
+        boolean continuar = true;
+        String tarefas[] = new String[10000];
+        String novaTarefa;
+        int cont = 1;
 
-    System.out.println("Digite suas tarefas, ao digitar Sair o programa encerrará");
+        System.out.println("\n===== Exercício 1 =====\n");
 
-    String tarefa = ler.nextLine();
+        while (continuar) {
 
-        
+            System.out.println("Informe a tarefa:");
+            novaTarefa = ler.nextLine();
+
+            tarefas = adicionar(tarefas, cont, novaTarefa);
+
+            continuar = novarodada(novaTarefa, continuar);
+
+            cont++;
+        }
+
+        System.out.println("\nSuas tarefas do dia são:");
+
+        exibir(tarefas, cont);
+
     }
+
+    public static String[] adicionar(String tarefas[], int cont, String novaTarefa) {
+
+        for (int i = (cont - 1); i < cont; i++) {
+            tarefas[cont-1] = cont + ". " + novaTarefa;
+        }
+        return tarefas;
+    }
+
+    public static boolean novarodada(String novaTarefa, boolean continuar) {
+
+        if (novaTarefa.equalsIgnoreCase("sair")) {
+            continuar = false;
+        }
+        return continuar;
+    }
+
+    public static void exibir(String tarefas[], int cont) {
+
+        for (int i = 0; i < (cont-2); i++) {
+            System.out.println(tarefas[i]);
+        }
+    }
+
 }
